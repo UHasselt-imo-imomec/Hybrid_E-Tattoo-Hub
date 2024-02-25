7d4955ab012dea73703126b058da5d6979894301
LaurenceJorissen <99961435+LaurenceJorissen@users.noreply.github.com>
2024-02-25 20:05:11 +0100

    Deze code uitgetest met Arduino maar niet met esp

--- ESP32_C3/src/main.cpp
+++ ESP32_C3/src/main.cpp
@@ -1,45 +1,15 @@
-//Test 
-
-  
-#if defined(ESP32)
-#include <WiFiMulti.h>
-WiFiMulti wifiMulti;
-#define DEVICE "ESP32"
-#elif defined(ESP8266)
-#include <ESP8266WiFiMulti.h>
-ESP8266WiFiMulti wifiMulti; //Demo
-#define DEVICE "ESP8266"
-#endif
-
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_BMP280.h>
 #include <MAX3010x.h>
 #include <hp_BH1750.h> 
 #include "filters.h"
-#include "secrets.h"
-#include <InfluxDbClient.h>
-#include <InfluxDbCloud.h>
 
-// WiFi AP SSID
-#define WIFI_SSID SSID
-// WiFi password
-#define WIFI_PASSWORD Password
-#define INFLUXDB_URL url
-#define INFLUXDB_TOKEN TOKEN
-#define INFLUXDB_ORG ORG
-#define INFLUXDB_BUCKET BUCKET
-// Time zone info
-#define TZ_INFO "UTC-1"
-
-// Declare InfluxDB client instance with preconfigured InfluxCloud certificate
-InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
-
-// Declare Data point
-Point X("Sensoren_data");
-
+#define TCAADDR 0x70
 
 #define BMP280_ADDRESS 0x76
+#define BH1750_ADDRESS 0x23
+#define MAX30100_ADDRESS 0x57
 Adafruit_BMP280 bmp; // I2C
 hp_BH1750 BH1750;       //  create the sensor
 
@@ -64,51 +34,61 @@ const bool kEnableAveraging = true;
 const int kAveragingSamples = 50;
 const int kSampleThreshold = 5;
 
-void setup() {
-  Wire.begin(8,10);
+void tcaselect(uint8_t i) {
+  if (i > 7) return;
+ 
+  Wire.beginTransmission(TCAADDR);
+  Wire.write(1 << i);
+  Wire.endTransmission();  
+}
+
+
+// standard Arduino setup()
+void setup()
+{
   Serial.begin(115200);
 
   delay(5000);
-
-  WiFi.setTxPower(WIFI_POWER_8_5dBm);
-  WiFi.mode(WIFI_STA);
-  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
-  
-  Serial.print("Connecting to wifi");
-  while (wifiMulti.run() != WL_CONNECTED) {
-    Serial.print(".");
-    delay(100);
-  }
-  Serial.println();
-  
-  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
-
-  if (client.validateConnection()) {
-    Serial.print("Connected to InfluxDB_1: ");
-    Serial.println(client.getServerUrl());
-  } else {
-    Serial.print("InfluxDB connection failed: ");
-    Serial.println(client.getLastErrorMessage());
-  }
-  Serial.println("\tAvailable RAM memory: " + String(esp_get_free_heap_size()) + " bytes");
-  
-  // Set write options for batching and precision
-  client.setWriteOptions(
-      WriteOptions()
-          .writePrecision(WritePrecision::MS)
-          .batchSize(10)
-          .bufferSize(1000)
-          .flushInterval(10)
-  );
-
-  // Set HTTP options for the client
-  client.setHTTPOptions(
-      HTTPOptions().connectionReuse(true)
-  );  
-
   Serial.println("Test");
-  while ( !Serial ) delay(100);   // wait for native usb
-  Serial.println(F("BMP280 test"));
+
+  Wire.begin();
+  Serial.println("\nTCAScanner ready!");
+  
+  for (uint8_t t=0; t<8; t++) {
+    tcaselect(t);
+    Serial.print("TCA Port #"); Serial.println(t);
+
+    for (uint8_t addr = 0; addr<=127; addr++) {
+      if (addr == TCAADDR) continue;
+
+      Wire.beginTransmission(addr);
+      if (!Wire.endTransmission()) {
+        Serial.print("Found I2C 0x");  Serial.println(addr,HEX);
+      }
+    }
+  }
+  Serial.println("\ndone");
+  Serial.println("alles effe testen g"); Serial.println("");
+  
+  /* Initialise the 1st sensor */
+  tcaselect(7);
+  if(!sensor.begin())
+  {
+    /* There was a problem detecting the HMC5883 ... check your connections */
+    Serial.println("Ooops, no PPG detected ... Check your wiring!");
+    while(1);
+  }
+  
+  /* Initialise the 2nd sensor */
+  tcaselect(5);
+  bool avail = BH1750.begin(BH1750_ADDRESS);// init the sensor with address pin connetcted to ground
+                                              // result (bool) wil be be "false" if no sensor found
+  if (!avail) {
+    Serial.println("No BH1750 sensor found!");
+    while (true) {}; 
+  }
+
+  tcaselect(6);
   unsigned status;
   status = bmp.begin(BMP280_ADDRESS);
   if (!status) {
@@ -127,23 +107,11 @@ void setup() {
                   Adafruit_BMP280::SAMPLING_X16,    
                   Adafruit_BMP280::FILTER_X16,      
                   Adafruit_BMP280::STANDBY_MS_500); 
-
   
-  bool avail = BH1750.begin(0x23);// init the sensor with address pin connetcted to ground
-                                              // result (bool) wil be be "false" if no sensor found
-  if (!avail) {
-    Serial.println("No BH1750 sensor found!");
-    while (true) {}; 
-  }
 
-  if(sensor.begin() && sensor.setSamplingRate(kSamplingRate)) { 
-    Serial.println("Sensor initialized");
-  }
-  else {
-    Serial.println("Sensor not found");  
-    while(1);
-  }
-  
+  Serial.println("Aight lets cook");
+  /* Display some basic information on this sensor */
+
 }
 
 // Filter Instances
@@ -164,7 +132,9 @@ float last_diff = NAN;
 bool crossed = false;
 long crossed_time = 0;
 
-void loop() {
+void loop() 
+{
+  tcaselect(7);
   auto sample = sensor.readSample(1000);
   float current_value = sample.red;
   
@@ -215,12 +185,38 @@ void loop() {
   
               // Show if enough samples have been collected
               if(averager.count() > kSampleThreshold) {
-                X.addField("average bpm", bpm);
+                Serial.print("Heart Rate (avg, bpm): ");
+                Serial.println(average_bpm);
               }
             }
             else {
-              X.addField("current bpm", bpm);  
+              Serial.print("Heart Rate (current, bpm): ");
+              Serial.println(bpm);  
             }
+              tcaselect(6);
+              float temp=bmp.readTemperature();
+              float pressure=bmp.readPressure();
+              float altitude=bmp.readAltitude(1013.25);
+                // Print the values
+              Serial.print("Temperature: ");
+              Serial.print(temp);
+              Serial.println(" *C");
+
+              Serial.print("Pressure: ");
+              Serial.print(pressure);
+              Serial.println(" Pa");
+
+              Serial.print("Altitude: ");
+              Serial.print(altitude);
+              Serial.println(" meters");
+
+              tcaselect(5);
+              BH1750.start();   //starts a measurement
+              float lux=BH1750.getLux();  //  waits until a conversion finished
+              Serial.print("LUX: ");
+              Serial.print(lux);
+              Serial.println(" ...");
+              Serial.println("");
           }
         }
         crossed = false;
@@ -229,85 +225,4 @@ void loop() {
     }
     last_diff = current_diff;
   }
-  if (client.isBufferEmpty()) {
-    Serial.println("Buffer is leeg g");
-  }
-  Serial.println("De hoeveelheid beschikbare RAM: " + String(esp_get_free_heap_size())  + "kb");
-  float temp=bmp.readTemperature();
-  float pressure=bmp.readPressure();
-  float altitude=bmp.readAltitude(1013.25);
-  BH1750.start();   //starts a measurement
-  float lux=BH1750.getLux();  //  waits until a conversion finished
-  X.addField("Temp", temp);
-  X.addField("Pressure", pressure);
-  X.addField("Altitude", altitude);
-  X.addField("Lux", lux);
-  // Write the point to InfluxDB
-  if (client.writePoint(X)) {
-    //Serial.println("Data sent to InfluxDB successfully!");
-    Serial.println("\tAvailable RAM memory: " + String(esp_get_free_heap_size()) + " bytes");
-  } else {
-    Serial.print("InfluxDB write failed: ");
-    Serial.println(client.getLastErrorMessage());
-  }
-  // Clear previous data from the point
-  X.clearFields();
 }
-
-/*
-//I2CScanner
-#include <Arduino.h>
-#include <Wire.h>
-
-
-void setup()
-{
-  Wire.begin(8,10); //SDA,SCL
-
-  Serial.begin(115200);
-  while (!Serial);             // Leonardo: wait for serial monitor
-  Serial.println("\nI2C Scanner");
-}
-
-
-void loop()
-{
-  byte error, address;
-  int nDevices;
-
-  Serial.println("Scanning...");
-
-  nDevices = 0;
-  for(address = 1; address < 127; address++ ){
-    // The i2c_scanner uses the return value of
-    // the Write.endTransmisstion to see if
-    // a device did acknowledge to the address.
-    Wire.beginTransmission(address);
-    error = Wire.endTransmission();
-    Serial.printf("0X%X, ", address, error);
-
-    if (error == 0){
-      Serial.print("\nI2C device found at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.print(address,HEX);
-      Serial.println("  !");
-
-      nDevices++;
-    }
-    else if (error==4){
-      Serial.print("\nUnknown error at address 0x");
-      if (address<16) 
-        Serial.print("0");
-      Serial.println(address,HEX);
-    }    
-  }
-
-  if (nDevices == 0)
-    Serial.println("\nNo I2C devices found\n");
-  else
-    Serial.println("done\n");
-
-  delay(5000);           // wait 5 seconds for next scan
-}
-*/
