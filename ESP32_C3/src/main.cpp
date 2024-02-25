03ac3c29988a3b937809d01f7336f91bdb20554b
LaurenceJorissen <99961435+LaurenceJorissen@users.noreply.github.com>
2024-02-25 19:57:56 +0100

    Dit werkt?

--- ESP32_C3/src/main.cpp
+++ ESP32_C3/src/main.cpp
@@ -1,8 +1,47 @@
-//Test clone
-//Laurence demo
+//Test 
+
+  
+#if defined(ESP32)
+#include <WiFiMulti.h>
+WiFiMulti wifiMulti;
+#define DEVICE "ESP32"
+#elif defined(ESP8266)
+#include <ESP8266WiFiMulti.h>
+ESP8266WiFiMulti wifiMulti; //Demo
+#define DEVICE "ESP8266"
+#endif
+
 #include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_BMP280.h>
 #include <MAX3010x.h>
+#include <hp_BH1750.h> 
 #include "filters.h"
+#include "secrets.h"
+#include <InfluxDbClient.h>
+#include <InfluxDbCloud.h>
+
+// WiFi AP SSID
+#define WIFI_SSID SSID
+// WiFi password
+#define WIFI_PASSWORD Password
+#define INFLUXDB_URL url
+#define INFLUXDB_TOKEN TOKEN
+#define INFLUXDB_ORG ORG
+#define INFLUXDB_BUCKET BUCKET
+// Time zone info
+#define TZ_INFO "UTC-1"
+
+// Declare InfluxDB client instance with preconfigured InfluxCloud certificate
+InfluxDBClient client(INFLUXDB_URL, INFLUXDB_ORG, INFLUXDB_BUCKET, INFLUXDB_TOKEN, InfluxDbCloud2CACert);
+
+// Declare Data point
+Point X("Sensoren_data");
+
+
+#define BMP280_ADDRESS 0x76
+Adafruit_BMP280 bmp; // I2C
+hp_BH1750 BH1750;       //  create the sensor
 
 // Sensor (adjust to your sensor type)
 MAX30105 sensor;
@@ -26,10 +65,76 @@ const int kAveragingSamples = 50;
 const int kSampleThreshold = 5;
 
 void setup() {
+  Wire.begin(8,10);
   Serial.begin(115200);
 
   delay(5000);
+
+  WiFi.setTxPower(WIFI_POWER_8_5dBm);
+  WiFi.mode(WIFI_STA);
+  wifiMulti.addAP(WIFI_SSID, WIFI_PASSWORD);
+  
+  Serial.print("Connecting to wifi");
+  while (wifiMulti.run() != WL_CONNECTED) {
+    Serial.print(".");
+    delay(100);
+  }
+  Serial.println();
+  
+  timeSync(TZ_INFO, "pool.ntp.org", "time.nis.gov");
+
+  if (client.validateConnection()) {
+    Serial.print("Connected to InfluxDB_1: ");
+    Serial.println(client.getServerUrl());
+  } else {
+    Serial.print("InfluxDB connection failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+  Serial.println("\tAvailable RAM memory: " + String(esp_get_free_heap_size()) + " bytes");
+  
+  // Set write options for batching and precision
+  client.setWriteOptions(
+      WriteOptions()
+          .writePrecision(WritePrecision::MS)
+          .batchSize(10)
+          .bufferSize(1000)
+          .flushInterval(10)
+  );
+
+  // Set HTTP options for the client
+  client.setHTTPOptions(
+      HTTPOptions().connectionReuse(true)
+  );  
+
   Serial.println("Test");
+  while ( !Serial ) delay(100);   // wait for native usb
+  Serial.println(F("BMP280 test"));
+  unsigned status;
+  status = bmp.begin(BMP280_ADDRESS);
+  if (!status) {
+    Serial.println(F("Could not find a valid BMP280 sensor, check wiring or "
+                      "try a different address!"));
+    Serial.print("SensorID was: 0x"); Serial.println(bmp.sensorID(),16);
+    Serial.print("        ID of 0xFF probably means a bad address, a BMP 180 or BMP 085\n");
+    Serial.print("   ID of 0x56-0x58 represents a BMP 280,\n");
+    Serial.print("        ID of 0x60 represents a BME 280.\n");
+    Serial.print("        ID of 0x61 represents a BME 680.\n");
+    while (1) delay(10);
+  }
+
+  bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,    
+                  Adafruit_BMP280::SAMPLING_X2,     
+                  Adafruit_BMP280::SAMPLING_X16,    
+                  Adafruit_BMP280::FILTER_X16,      
+                  Adafruit_BMP280::STANDBY_MS_500); 
+
+  
+  bool avail = BH1750.begin(0x23);// init the sensor with address pin connetcted to ground
+                                              // result (bool) wil be be "false" if no sensor found
+  if (!avail) {
+    Serial.println("No BH1750 sensor found!");
+    while (true) {}; 
+  }
 
   if(sensor.begin() && sensor.setSamplingRate(kSamplingRate)) { 
     Serial.println("Sensor initialized");
@@ -38,6 +143,7 @@ void setup() {
     Serial.println("Sensor not found");  
     while(1);
   }
+  
 }
 
 // Filter Instances
@@ -109,29 +215,46 @@ void loop() {
   
               // Show if enough samples have been collected
               if(averager.count() > kSampleThreshold) {
-                Serial.print("Heart Rate (avg, bpm): ");
-                Serial.println(average_bpm);
+                X.addField("average bpm", bpm);
               }
             }
             else {
-              Serial.print("Heart Rate (current, bpm): ");
-              Serial.println(bpm);  
+              X.addField("current bpm", bpm);  
             }
           }
         }
-  
         crossed = false;
         last_heartbeat = crossed_time;
       }
     }
-
     last_diff = current_diff;
   }
+  if (client.isBufferEmpty()) {
+    Serial.println("Buffer is leeg g");
+  }
+  Serial.println("De hoeveelheid beschikbare RAM: " + String(esp_get_free_heap_size())  + "kb");
+  float temp=bmp.readTemperature();
+  float pressure=bmp.readPressure();
+  float altitude=bmp.readAltitude(1013.25);
+  BH1750.start();   //starts a measurement
+  float lux=BH1750.getLux();  //  waits until a conversion finished
+  X.addField("Temp", temp);
+  X.addField("Pressure", pressure);
+  X.addField("Altitude", altitude);
+  X.addField("Lux", lux);
+  // Write the point to InfluxDB
+  if (client.writePoint(X)) {
+    //Serial.println("Data sent to InfluxDB successfully!");
+    Serial.println("\tAvailable RAM memory: " + String(esp_get_free_heap_size()) + " bytes");
+  } else {
+    Serial.print("InfluxDB write failed: ");
+    Serial.println(client.getLastErrorMessage());
+  }
+  // Clear previous data from the point
+  X.clearFields();
 }
 
-
 /*
-
 //I2CScanner
 #include <Arduino.h>
 #include <Wire.h>
@@ -187,5 +310,4 @@ void loop()
 
   delay(5000);           // wait 5 seconds for next scan
 }
-
 */
